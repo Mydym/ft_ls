99f2497ed2da172ee034c70f5fa9b8996d6f5988
Valentin Grenier <valentingrenier@hotmail.fr>
2016-04-05 18:14:31 +0200

    liste des fichiers d'un repo

--- libft/main.c
+++ libft/main.c
@@ -13,15 +13,14 @@ int		main(int argc, char **argv)
 	if ((list = (char **)malloc(512 * sizeof(char *))) != NULL)
 		if (argc == 2)
 			if ((doc = ft_opendir(argv[1])) != NULL)
+			{
 				while ((file = ft_readdir(doc)) != NULL)
 				{
 					list[i] = file;
-					ft_putnbr(i);
-					ft_putchar(' ');
-					ft_putendl(list[i]);
 					i++;
 				}
+				closedir(doc);
+			}
 	list[i] = NULL;
-	i = 0;
 	return (0);
 }
